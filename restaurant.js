19d87c2d3a863bfd8da6453cbc18857c62e0b824
seijitanimoto <seijitanimoto1997@gmail.com>
2024-02-16 11:09:23 +0900

    edit file

--- restaurant.js
+++ restaurant.js
@@ -35,3 +35,49 @@ restaurant.greet2();
 console.log("######This is debugging2. The process has progressed to this point.######")
 restaurant.info();
 console.log("######This is debugging3. The process has progressed to this point.######")
+
+
+// ---------------------------------------------
+
+const names = [
+  {firstName: "Kate", lastName: "Jones"},
+];
+
+// Create a new array for the constant names and assign it to the constant fullNames
+const fullNames = names.map((name) => {
+  return name.firstName + " " + name.lastName;
+});
+
+class Product{
+  constructor(name, menue, price){
+      //プロパティの初期化
+      this.name = name;
+      this.menue = menue;
+      this.price = price;
+  }
+
+  //各インスタンスから参照されるメソッドを定義
+  hi(){
+      console.log("いらっしゃいませ！");
+      console.log(`${fullNames}様`);
+  }
+
+  info(){
+  console.log(`${this.name}のおすすめ`);
+  console.log(`　◼　${this.menue}は${this.price}円です`);
+  }
+
+  end(){
+    console.log("お上がりよっっ！！！！");
+  }
+}
+//以下、インスタンスオブジェクトの生成
+const product1 = new Product("本日", "pasta", 1500);
+const product2 = new Product("季節", "pizza", 2000);
+const product3 = new Product("シェフ", "グラタン", 1000);
+
+product1.hi();
+product1.info();
+product2.info();
+product3.info();
+product1.end();
